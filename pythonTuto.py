b60cd82711f7791558bbe6984efeb2f3d6e7f7cb
psalcedodev <psalcedodev@gmail.com>
2020-11-16 22:28:17 -0700

    Python code

---
+++ pythonTuto.py
@@ -0,0 +1,33 @@
+# single line comment
+''' multiline comments ''' 
+
+'''
+# Create your Facebook profile
+string = "text" or 'text' | Text
+integer = 1234567890 | Numbers
+boolean = True or False
+'''
+
+firstName = "Nate"
+lastName = "Del Grande"
+relationship = "Married"
+location = "Saint George, UT"
+occupation = "Student"
+age = 24
+male = False
+female = True
+
+#############################################################
+# str()
+# int()
+# if / elif / else
+print("My Facebook profile")
+print("Name: " + firstName+ " " + lastName)
+print("Age: " + str(age))
+if(male == True):
+    print("Gender: Male")
+else:
+    print("Gender: Female")
+print("Relationship Status: " + relationship)
+print("Ocupation: " + occupation)
+#############################################################
